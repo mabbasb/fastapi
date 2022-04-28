b5ee1cba3c27145d777b33ba68db43d3755433b1
Abbas <murcielo.goda@gmail.com>
2022-04-28 12:51:59 +0800

    adding aruduino post

--- app/routers/arudino.py
+++ app/routers/arudino.py
@@ -15,4 +15,9 @@ router = APIRouter(
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.ArduinoData)
 def sensor_data(data: schemas.ArduinoData, db: Session = Depends(get_db)):
-    
+    new_data = models.Arduino(**data.dict())
+    db.add(new_data)
+    db.commit()
+    db.refresh(new_data)
+
+    return new_data
