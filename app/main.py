4099d9e4fe089318855eff673c37df5d060e8a7c
Abbas <murcielo.goda@gmail.com>
2022-04-28 13:39:48 +0800

    added arduino post

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from . import models
 from .database import engine
-from .routers import post, user, authen, vote
+from .routers import post, user, authen, vote, arudino
 from fastapi.middleware.cors import CORSMiddleware
 
 #models.Base.metadata.create_all(bind=engine)
@@ -22,6 +22,7 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(authen.router)
 app.include_router(vote.router)
+app.include_router(arudino.router)
 
 @app.get("/")
 def root():
