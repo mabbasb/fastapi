2ea4264540bcea0642db32230dbad9217a285c25
Abbas <murcielo.goda@gmail.com>
2022-05-12 15:54:37 +0800

    Debugged

--- app/routers/arudino.py
+++ app/routers/arudino.py
@@ -7,7 +7,7 @@ from .. import models
 router = APIRouter(prefix="/arduino",
     tags=['Arduino'])
 
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.ArduinoData)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Test)
 def sensor_data(data: schemas.Test, db: Session = Depends(get_db)):
     new_data = models.Test(**data.dict())
     db.add(new_data)
